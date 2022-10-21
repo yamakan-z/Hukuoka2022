18c00dc19fd14a5f45e552dc0c2c95a263dec816
DEV3-11\GA3 <h1202016006@hiratagakuen.onmicrosoft.com>
2022-10-21 15:59:15 +0900

    敵カードランダム取得　骨組み完成

--- Hukuoka2022/Assets/yusuke/Scripts/Raycast2D.cs
+++ Hukuoka2022/Assets/yusuke/Scripts/Raycast2D.cs
@@ -9,17 +9,27 @@ public class Raycast2D : MonoBehaviour
     [SerializeField]
     GameObject[] clickcard = new GameObject[2];//クリックしたカードを入れるゲームオブジェクト
 
+    [SerializeField]
+    GameObject[] enemycards;//敵が選択するカード
+
+    public GameObject enemyselect_card;//敵が選択したカード
+
     public GameObject Player1Canvans;//プレイヤー1キャンバス
 
     public GameObject Player2Canvas;//プレイヤー2キャンバス
 
     public GameObject BattleCanvas;//バトル画面を持ってくる
 
-    public int cardcost;//カードコスト
+    public GameObject CostManager;//コスト管理マネージャーを持ってくる
+
 
     public int Turn_flow = 1;//現在のプレイヤー　1：1P　2：2P 3:バトル画面
 
     public int[] clicknum;//選択したカードの数字を入れる変数
+
+    int r_num;//敵カード取得のためランダムな数字をとる
+
+    private bool rand = true;//ランダム処理を行うフラグ
 
     public bool judge;//勝敗判定を始めるフラグ
 
@@ -28,6 +38,11 @@ public class Raycast2D : MonoBehaviour
     {
         clicknum = new int[2];//配列の作成
         Player2Canvas.SetActive(false);//プレイヤー1からスタート
+    }
+
+    public void RandomCardSelect()
+    {
+        r_num = Random.Range(0, 5);//取得した敵カードをランダムで取得する
     }
 
     // Update is called once per frame
@@ -43,9 +58,26 @@ public class Raycast2D : MonoBehaviour
         {
             Player1Canvans.SetActive(false);
             Player2Canvas.SetActive(true);
+
+            //現在選択できる敵のカードを全部取得
+            GameObject[] enemycard = GameObject.FindGameObjectsWithTag("Card");
+
+            enemycards = enemycard;//デバッグ用にインスペクターに見えるようにする
+
+            //ランダム処理を行う
+            if(rand)
+            {
+                RandomCardSelect();
+                rand = false;//このターン1度だけランダム処理させる
+            }
+          
+
+            enemyselect_card = enemycard[r_num];
+
         }
         else if(Turn_flow == 3)//バトル画面
         {
+            rand = true;//これでまたランダム処理を行える
             Player2Canvas.SetActive(false);
             BattleCanvas.SetActive(true);
 
@@ -85,11 +117,24 @@ public class Raycast2D : MonoBehaviour
                 //1P
                 if(Turn_flow == 1)
                 {
-                    clickcard[0] = hit.collider.gameObject;//クリックしたカードを取得する
-                
-                    clicknum[0] = clickcard[0].GetComponent<CardManager>().cardnum;//取得したカードの数字をclicknumに代入
                    
-                    Turn_flow++;//2Pへターンを回す
+                    clickcard[0] = hit.collider.gameObject;//クリックしたカードを取得する
+
+                    //現在のコストが選択したカードの数字より多ければ出せる
+                    if (CostManager.GetComponent<CardCost>().cardcost >= clickcard[0].GetComponent<CardManager>().cardnum)
+                    {
+                        clicknum[0] = clickcard[0].GetComponent<CardManager>().cardnum;//取得したカードの数字をclicknumに代入
+
+                        CostManager.GetComponent<CardCost>().cardcost -= clicknum[0];
+
+                        Turn_flow++;//2Pへターンを回す
+                    }
+                    else
+                    {
+                        Debug.Log("コストが足りない！！");
+                    }
+
+                   
                 }
                 //2P
                 else if (Turn_flow == 2)
