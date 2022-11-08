2a7af8afba4afc16c734e4f53ba33ab5c7b1b550
佑亮 本山 <h1202015003@hiratagakuen.onmicrosoft.com>
2022-11-08 16:35:02 +0900

    アニメーション途中

--- Hukuoka2022/Assets/yusuke/Scripts/Animation/Scripts/AttackAnimation.cs
+++ Hukuoka2022/Assets/yusuke/Scripts/Animation/Scripts/AttackAnimation.cs
@@ -1,8 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class AttackAnimation : MonoBehaviour
 {
-    
+    [SerializeField] Animator AnimationImage = null;
+
+    public void ClickAttackButton()
+    {
+        AnimationImage.SetTrigger("AttackTrigger");
+        AnimationImage.SetTrigger("EmptyTrigger");
+    }
 }
